215f4784b6919f5e51fe88d6d0964ed9cf650d2c
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-07-29 01:40:03 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -137,7 +137,9 @@ def adding_to_dennis
         }
      }
 
-programmer_hash[:dennis_ritchie][:languages][0] = "Assembly"
+programmer_hash[:dennis_ritchie]={
+  
+  "languages" => "Assembly"}
 end
 
 #params[:user][:keys_attributes] = {
